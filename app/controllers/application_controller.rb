7791f7a8feb137499928a7ff301f08dc8e8da44e
C <pierrecammas31470@gmail.com>
2020-08-28 09:54:07 +0200

    -V OK

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,7 @@ class ApplicationController < ActionController::Base
     before_action :configure_devise_parameters, if: :devise_controller?
 
     def configure_devise_parameters
-<<<<<<< HEAD
-        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :avatar)}
-=======
         devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation)}
->>>>>>> parent of 419ce49... -- facebook test
     end
 
     
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   devise :omniauthable, :omniauth_providers => [:facebook]
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
 
   
 
